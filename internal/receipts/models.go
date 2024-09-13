0a0b6ab02776c45e148145f8ebba9a457a2b8e55
Timothy Herng-Yih Gan <timothygan@utexas.edu>
2024-09-13 12:55:33 -0700

    Refactored Items/ItemsDTO

--- internal/receipts/models.go
+++ internal/receipts/models.go
@@ -10,27 +10,35 @@ import "time"
 // Items: List of items purchased.
 // Total: The total amount paid on the receipt.
 type Receipt struct {
-	ID           string    `json:"id" binding:"required"`
-	Retailer     string    `json:"retailer" binding:"required"`
-	PurchaseDate time.Time `json:"purchaseDate" binding:"required"`
-	PurchaseTime time.Time `json:"purchaseTime" binding:"required"`
-	Items        []Item    `json:"items" binding:"required"`
-	Total        string    `json:"total" binding:"required"`
-}
-
-// ReceiptDTO - Data Transfer Object for a receipt
-type ReceiptDTO struct {
-	Retailer     string `json:"retailer" binding:"required"`
-	PurchaseDate string `json:"purchaseDate" binding:"required"`
-	PurchaseTime string `json:"purchaseTime" binding:"required"`
-	Items        []Item `json:"items" binding:"required"`
-	Total        string `json:"total" binding:"required"`
+	ID           string    `json:"id"`
+	Retailer     string    `json:"retailer"`
+	PurchaseDate time.Time `json:"purchaseDate"`
+	PurchaseTime time.Time `json:"purchaseTime"`
+	Items        []Item    `json:"items"`
+	Total        string    `json:"total"`
 }
 
 // Item
 // ShortDescription: The Short Product Description for the item.
-// Price: The total price paid for this item.
+// Price: The total price paid for this item in cents
 type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            int64  `json:"price"`
+}
+
+// ReceiptDTO - Data Transfer Object for a receipt
+type ReceiptDTO struct {
+	Retailer     string    `json:"retailer" binding:"required"`
+	PurchaseDate string    `json:"purchaseDate" binding:"required"`
+	PurchaseTime string    `json:"purchaseTime" binding:"required"`
+	Items        []ItemDTO `json:"items" binding:"required"`
+	Total        string    `json:"total" binding:"required"`
+}
+
+// ItemDTO
+// ShortDescription: The Short Product Description for the item.
+// Price: The total price paid for this item.
+type ItemDTO struct {
 	ShortDescription string `json:"shortDescription" binding:"required"`
 	Price            string `json:"price" binding:"required"`
 }
